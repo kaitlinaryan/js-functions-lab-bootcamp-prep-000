f7c2752f4ae17d053a82b309c3f8e1a4394f642f
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2017-12-20 19:24:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
 return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  return `Happy Holidays, ${name}!`;
+  return `Happy Holidays, ${name}!`
 }
-funtion happyHolidaysTo(holiday)(name) {
-  return `Happy ${holiday}, ${name}!`;
+function happyHolidayTo(holiday)(name) {
+  return `Happy ${holiday}, ${name}!`
 }
